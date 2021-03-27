0f02847e3e3ed7229564760a16f9c94cea236503
0xFC963F18DC21 <mfabdullah2002@gmail.com>
2021-03-27 14:13:51 +0000

    [ffa20] documented

--- rendezvous/rendezvous/backend/User.py
+++ rendezvous/rendezvous/backend/User.py
@@ -10,6 +10,13 @@ class User:
     __next_id: int = 0
 
     def __init__(self, first_name: str = "", last_name: str = "", username: str = "", spotify_access_token: str = ""):
+        """
+        Create a new user
+        :param first_name: User's first name
+        :param last_name: User's last name
+        :param username: User's username
+        :param spotify_access_token: User's spotify access token
+        """
         self.__user_id = User.__next_id
         User.__next_id += 1
 
@@ -39,13 +46,29 @@ class User:
         """
 
     def get_spotify_access_token(self):
+        """
+        Gets the user's spotify access token. BE CAREFUL WITH WHAT USES THIS API.
+        :return: User's spotify access token
+        """
         return self.__spotify_access_token
 
     def set_first_name(self, first_name: str = ""):
+        """
+        Set a new first name for this user
+        :param first_name: New first name for the user
+        """
         self.__first_name = first_name
 
     def set_last_name(self, last_name: str = ""):
+        """
+        Set a new last name for this user
+        :param last_name: New last name for this user
+        """
         self.__last_name = last_name
 
     def set_username(self, username: str = ""):
+        """
+        Set a new username for this user
+        :param username: New username for this user
+        """
         self.__username = username
