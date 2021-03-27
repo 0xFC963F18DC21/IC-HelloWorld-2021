371326b449743254988c5f3eddf1c00723143b14
yelun <yly19@ic.ac.uk>
2021-03-28 00:20:03 +0800

    feat: pages and appUser apps

---
+++ rendezvous/users/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+class AppUser(models.Model):
+    first_name = models.TextField()
+    last_name = models.TextField()
+    username = models.TextField()
+    spotify_access_token = models.TextField(blank = True, null = True)
