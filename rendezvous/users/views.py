07e31a0df5b5118edbd5a8d944c823e0aafca7e6
0xFC963F18DC21 <mfabdullah2002@gmail.com>
2021-03-28 08:37:52 +0100

    small tweak to actually use the username var in register

--- rendezvous/users/views.py
+++ rendezvous/users/views.py
@@ -97,7 +97,7 @@ def register(request):
             else:
                 form.save()
                 username = form.cleaned_data.get('username')
-                messages.success(request, f'Your account has been created! You can now login!')
+                messages.success(request, f'Your account ({username}) has been created! You can now login!')
                 return redirect('/login')
     else:
         form = UserRegisterForm()
